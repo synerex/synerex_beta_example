7365aa79ca88e7b35841ed6ed0c6fb6b0e3c664d
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2021-12-13 02:19:54 +0900

    update user_porovider and add sxutil

--- user_provider/go.mod
+++ user_provider/go.mod
@@ -5,9 +5,16 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/golang/protobuf v1.5.2
+	github.com/gorilla/websocket v1.4.2
 	github.com/synerex/synerex_api v0.5.1
 	github.com/synerex/synerex_beta_example/proto_order v0.0.0-00010101000000-000000000000
 	github.com/synerex/synerex_sxutil v0.7.0
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.28.0 // indirect
+	go.opentelemetry.io/otel v1.3.0
+	go.opentelemetry.io/otel/exporters/jaeger v1.3.0 // indirect
+	go.opentelemetry.io/otel/trace v1.3.0
 )
 
 replace github.com/synerex/synerex_beta_example/proto_order => ../proto_order
+
+replace github.com/synerex/synerex_sxutil => ../synerex_sxutil
--- user_provider/user_provider.go
+++ user_provider/user_provider.go
@@ -16,18 +16,25 @@ import (
 	api "github.com/synerex/synerex_api"
 	order "github.com/synerex/synerex_beta_example/proto_order"
 	sxutil "github.com/synerex/synerex_sxutil"
+
+	"github.com/gorilla/websocket"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var (
 	nodesrv         = flag.String("nodesrv", "127.0.0.1:9990", "Node ID Server")
 	localsx         = flag.String("local", "", "Local Synerex Server")
 	mu              sync.Mutex
+	tracer          trace.Tracer
 	sxServerAddress string
 )
 
 const orderChannel uint32 = 1 // just for private
 var lastMyOrder uint64
 
+// order supply from Services.
 func supplyOrderCallback(clt *sxutil.SXServiceClient, sp *api.Supply) {
 	od := &order.Order{}
 	dt := sp.Cdata.Entity
@@ -36,11 +43,24 @@ func supplyOrderCallback(clt *sxutil.SXServiceClient, sp *api.Supply) {
 		log.Printf("got order! %s %#v", sp.SupplyName, *od)
 		if sp.SupplyName == "SupplyOrder" { // check the order for me!
 			if sp.TargetId == lastMyOrder { // this is supply for my order
+				// create new span! from supply.
+				spanCtx := sxutil.UnmarshalSpanContextJSON(sp.ArgJson)
+				log.Printf("Get Context %s", sp.ArgJson)
+				//  use ctx to next..
+				_, span := tracer.Start(
+					trace.ContextWithRemoteSpanContext(context.Background(), spanCtx),
+					"receive:SupplyOrder",
+					trace.WithSpanKind(trace.SpanKindClient),
+				)
 				// list up orders for selection.
 				fmt.Printf("Get %d Items!\n", len(od.Items))
 				for i := range od.Items {
 					fmt.Printf("%d: %s, price:%d\n", i, od.Items[i].Name, od.Items[i].Price)
 				}
+				// send order information to WebSocketClients.
+
+				// is OK to end span?
+				span.End()
 			}
 		}
 	}
@@ -79,26 +99,182 @@ func sendDemand(client *sxutil.SXServiceClient) {
 	}
 	et, _ := proto.Marshal(&ord)
 
+	// start Trace!
+	ctx := context.Background()
+	var span trace.Span
+	ctx, span = tracer.Start(
+		ctx,
+		"notifyDemand:OrderDemand",
+		trace.WithSpanKind(trace.SpanKindClient),
+	)
+	log.Println("Context pln", ctx)
+	log.Printf("Context  %#v", ctx)
+	log.Printf("SpanFromContext  %#v", trace.SpanFromContext(ctx))
+	log.Printf("BaseSpan         %#v", span)
+	log.Printf("Context With Span %#v", trace.ContextWithSpan(ctx, span))
+	sc := span.SpanContext()
+	js, _ := sc.MarshalJSON()
+	log.Printf("MarshalJSON of SpanContext %s", string(js))
+
 	opts := &sxutil.DemandOpts{
 		Name: "OrderDemand",
+		JSON: string(js),
 		Cdata: &api.Content{
 			Entity: et,
 		},
+		Context: ctx,
 	}
+	span.AddEvent("beforeSending")
 
 	lastMyOrder, _ = client.NotifyDemand(opts)
+	span.AddEvent("afterSending")
 	log.Printf("Send Demand %#v", opts)
+	log.Printf("SpanEnd! %v", span)
+	log.Printf("SpanEnd! %#v", span)
+	span.End()
 
 }
 
-func servWS(ts *TServ, w http.ResponseWriter, r *http.Request) {
+/* Websocket clients */
+
+type WClient struct {
+	ws   *WSServ
+	conn *websocket.Conn
+	send chan []byte
+}
+
+type WSServ struct {
+	clients    map[*WClient]bool // connected cilents
+	register   chan *WClient
+	unregister chan *WClient
+	broadcast  chan []byte
+}
+
+func NewWSServ() *WSServ {
+	return &WSServ{
+		broadcast:  make(chan []byte),
+		clients:    make(map[*WClient]bool),
+		register:   make(chan *WClient),
+		unregister: make(chan *WClient),
+	}
+}
+
+// main goroutine for Terminal server
+func (ws *WSServ) run() {
+
+	for {
+		select {
+		case client := <-ws.register:
+			ws.clients[client] = true
+		case client := <-ws.unregister:
+			if _, ok := ws.clients[client]; ok {
+				delete(ws.clients, client)
+				close(client.send)
+			}
+		case message := <-ws.broadcast: // broadcasting to all clients.
+			for client := range ws.clients {
+				select { // non-blocking send
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(ws.clients, client)
+				}
+			}
+		}
+	}
+}
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+func (ts *WSServ) messageHandler(msg []byte) {
+	// handling message from client
+	str := string(msg)
+	log.Printf("get message[%s]", str)
+
+}
+
+// read client message each terminals.
+func (c *WClient) readPump() {
+	defer func() {
+		c.ws.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("TS websocket error: %v", err)
+			}
+			break
+		}
+		go c.ws.messageHandler(message)
+	}
+}
+func (c *WClient) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			n := len(c.send) //
+			for i := 0; i < n; i++ {
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func servWS(ws *WSServ, w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	client := &TClient{ts: ts, conn: conn, send: make(chan []byte, 256)}
-	ts.register <- client
+	client := &WClient{ws: ws, conn: conn, send: make(chan []byte, 256)}
+	ws.register <- client
 
 	// Allow collection of memory referenced by the caller by doing all work in
 	// new goroutines.
@@ -115,6 +291,16 @@ func main() {
 
 	var rerr error
 	sxServerAddress, rerr = sxutil.RegisterNode(*nodesrv, "UserProvider", channelTypes, nil)
+
+	tp := sxutil.NewOltpTracer() // this enables default tracer!
+	defer func() {
+		log.Printf("Closing oltp traceer")
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Can't shoutdown tracer %v", err)
+		}
+	}()
+	tracer = otel.Tracer("user_provider", trace.WithInstrumentationVersion("0.0.1"))
+	log.Printf("Tracer generated %#v", tracer)
 
 	if rerr != nil {
 		log.Fatal("Can't register node:", rerr)
@@ -149,9 +335,10 @@ func main() {
 		sendDemand(geClient)
 	})
 
+	ws := NewWSServ() // web socket server
 	// for websocket
 	router.GET("/ws", func(c *gin.Context) {
-		serveWS(ts, c.Writer, c.Request)
+		servWS(ws, c.Writer, c.Request)
 	})
 
 	router.Run(":8081")
